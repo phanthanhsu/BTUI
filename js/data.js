24171c2cb393ad748e5695bbfb12abdd40d6ca67
Phan Thanh Su (FSU2.BU11) <SuPT1@fsoft.com.vn>
2018-03-06 17:03:16 +0700

    add js

---
+++ js/data.js
@@ -0,0 +1,51 @@
+var data = [{
+        "Group": "Group A",
+        "Driver": [{
+                "Name": "DriverNewbie",
+                "Distance": "20",
+                "Speed": "70",
+                "Time": "30",
+                "Idling": "22"
+            },
+            {
+                "Name": "DriverOld",
+                "Distance": "50",
+                "Speed": "40",
+                "Time": "60",
+                "Idling": "72"
+            },
+            {
+                "Name": "DriverSuper",
+                "Distance": "10",
+                "Speed": "140",
+                "Time": "20",
+                "Idling": "90"
+            }
+        ]
+    },
+    {
+        "Group": "Group B",
+        "Driver": [{
+                "Name": "DriverNgao",
+                "Distance": "99",
+                "Speed": "99",
+                "Time": "99",
+                "Idling": "99"
+            },
+            {
+                "Name": "DriverPhe",
+                "Distance": "88",
+                "Speed": "100",
+                "Time": "10",
+                "Idling": "43"
+            },
+            {
+                "Name": "DriverBay",
+                "Distance": "10",
+                "Speed": "100",
+                "Time": "10",
+                "Idling": "100"
+            }
+        ]
+    }
+];
